580f367a744ead3c0a9fb4358b4d151ae1fee80a
briandkruse <bkruse2@madisoncollege.edu>
2017-11-08 15:41:27 -0600

    Lots of updates for hibernate, daos, tests

--- src/main/java/edu.matc.incarcerationanalyzer/persistence/FacilityDao.java
+++ src/main/java/edu.matc.incarcerationanalyzer/persistence/FacilityDao.java
@@ -4,6 +4,7 @@ import edu.matc.incarcerationanalyzer.entity.Facility;
 import org.apache.log4j.Logger;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,4 +18,70 @@ public class FacilityDao {
         session.close();
         return facilities;
     }
+
+    public Facility getFacility(int id) {
+        Facility facility= null;
+        Session session = null;
+        try {
+            session = SessionFactoryProvider.getSessionFactory().openSession();
+            facility = (Facility) session.get(Facility.class, id);
+        } catch (HibernateException he) {
+            log.error("Error getting facility with id: " + id, he);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return facility;
+    }
+
+    public Facility addFacility(Facility facility) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = SessionFactoryProvider.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(facility);
+            transaction.commit();
+        } catch (HibernateException he){
+            if (transaction != null) {
+                try {
+                    transaction.rollback();
+                } catch (HibernateException he2) {
+                    log.error("Error rolling back save of facility: " + facility, he2);
+                }
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return facility;
+    }
+
+    public void deleteFacility(Facility facility) {
+
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = SessionFactoryProvider.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.delete(facility);
+            transaction.commit();
+        } catch (HibernateException he){
+            if (transaction != null) {
+                try {
+                    transaction.rollback();
+                } catch (HibernateException he2) {
+                    log.error("Error rolling back delete of facility id: " + facility, he2);
+                }
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+
+    }
 }
+
--- src/test/java/edu/matc/incarcerationanalyzer/persistence/FacilityDaoTest.java
+++ src/test/java/edu/matc/incarcerationanalyzer/persistence/FacilityDaoTest.java
@@ -1,7 +1,12 @@
 package edu.matc.incarcerationanalyzer.persistence;
 
+import edu.matc.incarcerationanalyzer.entity.Agepop;
+import edu.matc.incarcerationanalyzer.entity.Ethnicitypop;
 import edu.matc.incarcerationanalyzer.entity.Facility;
 //import org.apache.log4j.Logger;
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
@@ -10,15 +15,56 @@ import static org.junit.Assert.*;
 
 public class FacilityDaoTest {
 
-   // private final Logger log = Logger.getLogger(this.getClass());
-    FacilityDao facilityDao = new FacilityDao();
+   private final Logger log = Logger.getLogger(this.getClass());
+
+    FacilityDao facilityDao;
     int initialFacilityCount;
+    Facility facility;
+    @Before
+    public void facilitySetup() {
+        facilityDao = new FacilityDao();
+        initialFacilityCount = facilityDao.getAllFacilities().size();
 
-  /*  @Test
-    public void getAllFacilities() throws Exception {
+        //create test facility
+        facility = new Facility();
+        Agepop agepop = new Agepop(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+        Ethnicitypop ethnicitypop = new Ethnicitypop(1, 2, 3, 4);
+        facility.setName("TestFacility");
+        facility.setPopulation(50);
+        facility.setAgepop(agepop);
+        facility.setEthnicitypop(ethnicitypop);
+    }
 
+    @After
+    public void cleanup() {
+        facilityDao.deleteFacility(facility);
+    }
+
+    @Test
+    public void getAllFacilitiesTest() throws Exception {
         List<Facility> facilities = facilityDao.getAllFacilities();
         assertTrue("Failed to get all facilities" + facilities.size(), facilities.size() > 0);
-    }*/
+    }
+
+    @Test
+    public void getFacilityTest() throws Exception {
+        Facility facility = facilityDao.getFacility(1);
+        assertNotNull("User usertest was not found", facility);
+    }
+
+    @Test
+    public void addFacility() throws Exception {
+        facilityDao.addFacility(facility);
+        assertEquals("Facility was not added to the database", initialFacilityCount +1, facilityDao.getAllFacilities().size());
+    }
+
+    @Test
+    public void deleteFacility() throws Exception {
+        facilityDao.addFacility(facility);
+        initialFacilityCount = facilityDao.getAllFacilities().size();
+        facilityDao.deleteFacility(facility);
+        assertEquals("Facility was not deleted from the database", initialFacilityCount - 1, facilityDao.getAllFacilities().size());
+    }
+
 
 }
